63c143f651e1a9f8da36a78a700556ba57c44918
Florian Weingartshofer <florian.weingartshofer@dynatrace.com>
2021-09-01 10:09:42 +0200

    Update Person Class

--- test1.java
+++ test1.java
@@ -1,11 +1,26 @@
+import java.time.LocalDate;
+import java.util.UUID;
+
 public class Person {
-    public final UUID id;
-    public final String name;
-    public final LocalDate birthdate;
+    private final UUID id;
+    private final String name;
+    private final LocalDate birthdate;
 
     public Person(String name, LocalDate birthdate) {
         this.name = name;
         this.birthdate = birthdate;
         id = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
     }
 }
